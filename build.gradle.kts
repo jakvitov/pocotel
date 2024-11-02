6019658e91b754b22a9ac337f0b431b07bf9e66c
Jakub Vítovec <jakvitov@gmail.com>
2024-11-02 16:34:32 +0100

    Sqlite database

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.mariadb.jdbc:mariadb-java-client:3.5.0")
+	implementation("org.xerial:sqlite-jdbc:3.47.0.0")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
--- src/main/resources/db/init_db.sql
+++ src/main/resources/db/init_db.sql
@@ -1,9 +1,12 @@
-CREATE SEQUENCE STUDENT_ID_SEQ MINVALUE 0 INCREMENT BY 1;
-
 CREATE TABLE STUDENT (
-    id VARCHAR(255),
-    name VARCHAR(255),
-    surname VARCHAR(255),
-    born DATE,
-    averageGrade VARCHAR(255)
+                         id VARCHAR(255),
+                         name VARCHAR(255),
+                         surname VARCHAR(255),
+                         born DATE,
+                         averageGrade VARCHAR(255)
 );
+
+INSERT INTO STUDENT(id, name, surname, born, averageGrade) VALUES (1, 'John', 'Doe', '2001-02-15', '2.52141');
+INSERT INTO STUDENT(id, name, surname, born, averageGrade) VALUES (1, 'Max', 'Mustermann', '2004-04-31', '1.55241');
+INSERT INTO STUDENT(id, name, surname, born, averageGrade) VALUES (1, 'Mario', 'Rossi', '1995-11-3', '1.21');
+INSERT INTO STUDENT(id, name, surname, born, averageGrade) VALUES (1, 'Ivan', 'Ivanovich', '1999-12-12', '4.41125121');
