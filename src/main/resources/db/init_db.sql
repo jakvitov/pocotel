bdb504b6223909ca4441216d4c5da0f0e322cc9a
Jakub Vítovec <jakvitov@gmail.com>
2024-11-02 16:43:09 +0100

    Application setup finished

--- src/main/resources/db/init_db.sql
+++ src/main/resources/db/init_db.sql
@@ -3,10 +3,10 @@ CREATE TABLE STUDENT (
                          name VARCHAR(255),
                          surname VARCHAR(255),
                          born DATE,
-                         averageGrade VARCHAR(255)
+                         average_grade VARCHAR(255)
 );
 
-INSERT INTO STUDENT(id, name, surname, born, averageGrade) VALUES (1, 'John', 'Doe', '2001-02-15', '2.52141');
-INSERT INTO STUDENT(id, name, surname, born, averageGrade) VALUES (1, 'Max', 'Mustermann', '2004-04-31', '1.55241');
-INSERT INTO STUDENT(id, name, surname, born, averageGrade) VALUES (1, 'Mario', 'Rossi', '1995-11-3', '1.21');
-INSERT INTO STUDENT(id, name, surname, born, averageGrade) VALUES (1, 'Ivan', 'Ivanovich', '1999-12-12', '4.41125121');
+INSERT INTO STUDENT(id, name, surname, born, average_grade) VALUES (1, 'John', 'Doe', '2001-02-15', '2.52141');
+INSERT INTO STUDENT(id, name, surname, born, average_grade) VALUES (1, 'Max', 'Mustermann', '2004-04-30', '1.55241');
+INSERT INTO STUDENT(id, name, surname, born, average_grade) VALUES (1, 'Mario', 'Rossi', '1995-11-03', '1.21');
+INSERT INTO STUDENT(id, name, surname, born, average_grade) VALUES (1, 'Ivan', 'Ivanovich', '1999-12-12', '4.41125121');
