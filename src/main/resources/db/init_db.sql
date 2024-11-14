1076656246de8ee5c63145a81cbd16f90f35558b
Jakub Vítovec <jakvitov@gmail.com>
2024-11-14 20:50:55 +0100

    Init db fix

--- src/main/resources/db/init_db.sql
+++ src/main/resources/db/init_db.sql
@@ -1,3 +1,4 @@
+BEGIN;
 CREATE TABLE STUDENT (
                          id SERIAL PRIMARY KEY,
                          name VARCHAR(255),
@@ -11,3 +12,4 @@ INSERT INTO STUDENT(name, surname, born, average_grade) VALUES ('John', 'Doe', '
 INSERT INTO STUDENT(name, surname, born, average_grade) VALUES ('Max', 'Mustermann', '2004-04-30', '1.55241');
 INSERT INTO STUDENT(name, surname, born, average_grade) VALUES ('Mario', 'Rossi', '1995-11-03', '1.21');
 INSERT INTO STUDENT(name, surname, born, average_grade) VALUES ('Ivan', 'Ivanovich', '1999-12-12', '4.41125121');
+COMMIT;
